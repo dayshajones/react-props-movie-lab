24c7836e3015c7e99663819d7d7d64dac682b8df
“Daysha <+>
2022-03-29 18:07:28 -0700

    commit tree d11db6598fdcb64e59bcb4f2a51648b5910b31db

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -5,7 +5,11 @@ import movieData from './data.js'
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
-    // map over your movieData array and return an array of the correct JSX
+    return movieData.map((data, idx) => <MovieCard key={idx} 
+                            poster={data.poster} 
+                            title={data.title} 
+                            IMDBRating = {data.IMDBRating}
+                            genres = {data.genres} />);
   }
 
   render() {
